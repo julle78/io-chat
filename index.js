987fa2b3ddc5eaad2bdad1cb7325cdc922e6084a
Juhani Lahti <juhani.lahti@gmail.com>
2015-09-29 20:29:51 +0300

    added emit

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-      console.log('message ' + msg );
-});
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function(){
